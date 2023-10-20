0b6d73686c4241e3241ca6d7e1b2322f4394cda9
unknown <evgenrus70@mail.ru>
2023-10-20 16:30:11 +0700

    Net class added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+
+
+class Net {
+    public: 
+        std::string name;
+        std::string weightsPath;
+        std::string inputPath;    
+        std::string maskPath;
+
+        Net (std::string name,std::string weightsPath,std::string inputPath,std::string maskPath) {
+            
+        }      
+};
+
+
 int main () {
-    std::cout <<"Hello, world" <<std::endl;
+    std::cout <<"Start U-Net CNN" <<std::endl;
     return 0;
-}
+} 

6dcbf3b437a79aca9a771942632ea2a0b7c39738
Мыцко Евгений Алексеевич <evgenvt@tpu.ru>
2023-10-20 16:58:14 +0700

    Net class usage

--- main.cpp
+++ main.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
-
-
-
-class Net {
-    public: 
-        std::string name;
-        std::string weightsPath;
-        std::string inputPath;    
-        std::string maskPath;
-
-        Net (std::string name,std::string weightsPath,std::string inputPath,std::string maskPath) {
-            
-        }      
-};
-
+#include "Net.h"
 
 int main () {
     std::cout <<"Start U-Net CNN" <<std::endl;
+    Net unet = Net("unet","path/weights","path/inputs","path/mask");
+    unet.print();
     return 0;
 } 
